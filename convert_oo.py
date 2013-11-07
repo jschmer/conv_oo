d5ba4a766df1cebd43a1ac796fdab5e05338712f
Jens Schmer <der.liggers@gmail.com>
2013-11-08 00:30:10 +0100

    using argparse module instead of getopt

--- convert_oo.py
+++ convert_oo.py
@@ -1,20 +1,19 @@
 import os
 import fnmatch
 import sys
-import getopt
+import argparse
 
-def usage():
-    print('LibreOffice soffice.exe must be in path!')
-    print('')
-    print('convert_oo.py --dir "DV-Recht" --in-file-filter "*.odp" --out-type "pdf" --out-dir "pdf"')
-    print('')
-    print('--out-dir is relative to --dir if it\'s a relative path')
+parser = argparse.ArgumentParser(description="""
+Convert media files (for example odp -> pdf)
+>>> The soffice executable of LibreOffice (tested) or OpenOffice (untested) has to be in PATH!
+""",
+formatter_class=argparse.RawTextHelpFormatter)
+parser.add_argument("-s", "--source", action='store', nargs=1, default=".", type=str, help="Source directory where your media files are stored")
+parser.add_argument("-ff", "--file-filter", action='store', nargs=1, default="*.odp", type=str, help="Convert only the files in source that matches this filter, for example '*.odp'")
+parser.add_argument("-o", "--out-dir", action='store', nargs=1, default=".", type=str, help="Directory to hold the converted files\nIs relative to source if a relative path is given")
+parser.add_argument("-t", "--type", action='store', nargs=1, default="pdf", type=str, help="Media type to convert to, for example 'pdf'")
 
-def quote(path):
-  if path[0] != path[len(path)-1]:
-    return '"' + path + '"'
-  else:
-    return path
+args = parser.parse_args()
     
 def which(program):
     '''
@@ -36,48 +35,41 @@ def which(program):
 
     return None
 
-if which("soffice.exe") == None:
-    usage()                         
-    sys.exit(2)    
 
-# default arguments
-base_dir = "."
-input_file_filter = "*.odp"
-output_format = "pdf"
-outdir = "."
-     
-try:                                
-    opts, args = getopt.getopt(sys.argv[1:], "hd:i:t:o:", ["help", "dir=", "in-file-filter=", "out-type=", "out-dir="])
-except getopt.GetoptError:          
-    usage()                         
-    sys.exit(2)                     
-for opt, arg in opts:
-    if opt in ("-h", "--help"):
-        usage()
-        sys.exit(1)
-    elif opt in ("-d", "--dir"):
-        base_dir = os.path.abspath(arg)
-    elif opt in ("-i", "--in-file-filter"):
-        input_file_filter = arg              
-    elif opt in ("-t", "--out-type"):
-        output_format = arg          
-    elif opt in ("-o", "--out-dir"):
-        outdir = arg
+def quote(path):
+  if path[0] != path[len(path)-1]:
+    return '"' + path + '"'
+  else:
+    return path
+
+if which("soffice.exe") == None:
+    parser.print_help()
+    sys.exit(2)
 
 # conversion command: "soffice --headless --convert-to pdf --outdir #outdir# #file1# #file2# ... #fileN#"
 
+source = os.path.abspath(args.source[0])
+if not os.path.isdir(source):
+    print("ERROR: source is not a directory: args.source")
+    sys.exit(-1)
+
 # get all odp files in base_dir
 files = []
-for root, dirnames, filenames in os.walk(base_dir):
-  for filename in fnmatch.filter(filenames, input_file_filter):
+for root, dirnames, filenames in os.walk(source):
+  for filename in fnmatch.filter(filenames, args.file_filter[0]):
       files.append('"' + os.path.join(root, filename) + '"')
 
-# prepare outdir relative to base_dir
-if os.path.isabs(outdir):
-  outdir = '"' + outdir + '"'
-else:
-  outdir = '"' + os.path.abspath(os.path.join(base_dir, outdir)) + '"'
+if len(files) < 1:
+    print("Nothing to convert, aborting...")
+    sys.exit(0)
 
-cmd = "soffice.exe --headless --convert-to " + output_format + " --outdir " + outdir + " " + " ".join(files)
+# prepare outdir relative to base_dir
+if os.path.isabs(args.out_dir[0]):
+  outdir = quote(args.out_dir[0])
+else:
+  outdir = quote(os.path.abspath(os.path.join(source, args.out_dir[0])))
+
+cmd = "soffice.exe --headless --convert-to " + args.type[0] + " --outdir " + outdir + " " + " ".join(files)
+print(cmd)
 
 os.system(cmd)
